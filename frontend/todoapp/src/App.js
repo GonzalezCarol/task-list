936a4d498eda01d657484c51f9da18f651b68291
Carolina Alvarenga <carolina.gonzalez.alvarenga@gmail.com>
2021-04-28 16:18:24 -0300

    building the frontend structure

---
+++ frontend/todoapp/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MainPage from "./styles/main";
+
+function App() {
+  return (
+    <MainPage>
+      <h1>aqui vai ser verde</h1>
+    </MainPage>
+  );
+}
+
+export default App;
---
+++ frontend/todoapp/src/styles/main.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+const MainPage = styled.div`
+  background-color: red;
+`;
+export default MainPage;
